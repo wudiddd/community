662142f20ebc3e1bc666abe6c620a2d2a626f860
wudiddd <2395443033@qq.com>
2021-06-17 17:57:21 +0800

    init project

---
+++ README.md
@@ -0,0 +1,3 @@
+##
+ git commit --amend --no-edit 
+ 只会有一次commit记录，使用上次commit的记录
---
+++ src/test/java/com/yang/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yang.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
